eaa3ccae592c3a562af7a3f6aadd6c1b00228293
Eli-b-good <eli.i.henderson95@gmail.com>
2022-02-01 00:32:54 -0500

    UPDATE: login and register routes

--- models/User.js
+++ models/User.js
@@ -18,6 +18,37 @@ const userSchema = mongoose.Schema(
   }
 );
 
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (matchPass) {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(matchPass, user.password, (err, isMatch) => {
+      if (err) return reject(err);
+
+      if (!isMatch) return reject(false);
+
+      resolve(true);
+    });
+  });
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
